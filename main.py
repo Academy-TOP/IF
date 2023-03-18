3eb9a52c766f981b7684305c0369c220da32465b
Евгений Тищук <36456429+Jack36VRN@users.noreply.github.com>
2023-03-18 11:25:28 +0000

    12342354

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 var_1 = 5
-var_2 = 2
+var_2 = 3
 print(var_1 + var_2)
 print(var_1 - var_2)
 print(var_1 * var_2)
 print(var_1 / var_2)
-print(var_1 ** var_2)
+print(var_1**var_2)
 print(var_1 // var_2)
 print(var_1 % var_2)
+
+print('\nСложение списков и кортежей')
+list_1 = [1,1]
+list_2 = [2,2]
+tuple_1 = 3,3
+tuple_2 = 4,4
+print(list_1 + list_2)
+print(tuple_1 + tuple_2)
+print(list_1 * 2)
+print(tuple_1 * 2)
