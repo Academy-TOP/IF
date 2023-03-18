763290633575330fb43feb1b7914fc7d4cd15861
Евгений Тищук <36456429+Jack36VRN@users.noreply.github.com>
2023-03-18 11:29:23 +0000

    варпарпаопро

--- main.py
+++ main.py
@@ -15,5 +15,19 @@ tuple_1 = 3,3
 tuple_2 = 4,4
 print(list_1 + list_2)
 print(tuple_1 + tuple_2)
-print(list_1 * 2)
-print(tuple_1 * 2)
+print(list_1 * 3)
+print(tuple_1 * 3)
+
+print('\nУсловные операции')
+var_1 = 2
+var_2 = 1.1
+var_3 = 3
+var_4 = 3
+print(var_1 > var_2)
+print(var_1 < var_2)
+print(var_1 >= var_2)
+print(var_1 <= var_2)
+print(var_3 >= var_4)
+print(var_3 <= var_4)
+print(var_1 != var_2)
+print(var_3 == var_4)
